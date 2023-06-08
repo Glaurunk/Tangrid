d83344d2fac49cb49a971e8b1d970d206565b6a6
Glaurunk <glaurunk@yahoo.com>
2023-06-08 16:44:49 +0300

    update system requirements

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 // Abords the game on small screens
 function DetectDisplay()
 {
-    console.log(window.innerWidth);
-    console.log(window.innerHeight);
-    if (window.innerWidth < 1280 || window.innerHeight < 900) {
-        console.log('Inside');
-        const cont = document.querySelector('.container');
-        cont.innerHTML = "";
-        const msg = document.createElement('DIV');
-        msg.classList.add("small-window-msg");
-        msg.innerHTML = "The window of your browser is too small. This game requires a window of minimum 1280 X 900 pixels and a modern browser. Please try loading this page on a up-to-date desktop browser";
-        cont.appendChild(msg);
+    if (window.innerWidth < 1280 || window.innerHeight < 800) {
+        return false;
+    } else {
+        return true;
     }
 }
 
@@ -45,28 +39,39 @@ function CheckSettings()
 ////// INITIALIZATION
 ///////////////////////////////////////////////////////
 
-DetectDisplay();
-Clear();
-DisplayYear();
-// shuffle deck and draw cards
-PopulatePlayersHand();
-DrawGrid();
-DrawPlayerHand(0);
-// listen for user input
-window.addEventListener('keypress', function(e) { 
-    KeyInputHandler(e.key)
-});
-
-// Initialize the UI
-InitUI();
-// Check and apply current settings
-CheckSettings();
-
-// populate arrays of elements drawn by JS after the Draw functions
-cards = Array.from(document.getElementsByClassName('card'));
-
-// if a restart has been requested start the game immediately
-if (GetCookie('reload') === 'true') { 
-    console.log('OK');
-    startBtn.dispatchEvent(new Event('click'));
+const systemRequirements = DetectDisplay();
+if (systemRequirements) {
+    Clear();
+    DisplayYear();
+    // shuffle deck and draw cards
+    PopulatePlayersHand();
+    DrawGrid();
+    DrawPlayerHand(0);
+    // listen for user input
+    window.addEventListener('keypress', function(e) { 
+        KeyInputHandler(e.key)
+    });
+    
+    // Initialize the UI
+    InitUI();
+    // Check and apply current settings
+    CheckSettings();
+    
+    // populate arrays of elements drawn by JS after the Draw functions
+    cards = Array.from(document.getElementsByClassName('card'));
+    
+    // if a restart has been requested start the game immediately
+    if (GetCookie('reload') === 'true') { 
+        console.log('OK');
+        startBtn.dispatchEvent(new Event('click'));
+    }
+} else {
+    console.log('width: ' + window.innerWidth);
+    console.log('height:' + window.innerHeight);
+    const body = document.getElementById('body');
+    body.innerHTML = "";
+    const msg = document.createElement('DIV');
+    msg.classList.add("small-window-msg");
+    msg.innerHTML = "The window of your browser is too small. This version of Tangrid is designed only for desktop computers. The game requires a screen resolution of minimum 1280 X 900 pixels and a modern browser. Please try loading this page on a up-to-date desktop browser";
+    body.appendChild(msg);
 }
